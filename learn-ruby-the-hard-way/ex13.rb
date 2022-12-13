0ea4a177878911b6abc88d92ba9f1c189fe106f6
shawn-bachlet <shawn.bachlet@gocaribou.com>
2022-12-13 11:45:57 -0500

    Add exercise 13

---
+++ learn-ruby-the-hard-way/ex13.rb
@@ -0,0 +1,5 @@
+first, second, third = ARGV # not compatible with gets.chomp you need to use $stdin.gets.chomp
+
+puts "Your first variable is: #{first}"
+puts "Your second variable is: #{second}"
+puts "Your third variable is: #{third}"
